fa57ac52fd36223f01ee10e0247396eab112477c
Tomas Pulmano <tomas@scopely.com>
2015-02-19 10:54:03 -0800

    use scopely Log class

--- mopub-sdk/src/main/java/com/mopub/mobileads/MraidActivity.java
+++ mopub-sdk/src/main/java/com/mopub/mobileads/MraidActivity.java
@@ -8,7 +8,6 @@ import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
