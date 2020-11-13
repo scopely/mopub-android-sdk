a14900c90440019763750e50dce837ddbdaed25f
Carlos Castillo <carloscastillo@scopely.com>
2020-11-13 18:08:14 +0100

    Modified attempt succeeded events to return all the impression data

--- mopub-sdk/mopub-sdk-fullscreen/src/main/java/com/mopub/mobileads/RewardedVideoCustomEventAdListener.java
+++ mopub-sdk/mopub-sdk-fullscreen/src/main/java/com/mopub/mobileads/RewardedVideoCustomEventAdListener.java
@@ -1,7 +1,9 @@
 package com.mopub.mobileads;
 
+import com.mopub.network.ImpressionData;
+
 public interface RewardedVideoCustomEventAdListener {
     void onCustomEventRewardedVideoAttempted(String adUnitId, String customEventClass, String lineItemId);
-    void onCustomEventRewardedVideoAttemptSucceeded(String adUnitId, String creativeId, Double publisherRevenue);
+    void onCustomEventRewardedVideoAttemptSucceeded(String adUnitId, String creativeId, ImpressionData impressionData);
     void onCustomEventRewardedVideoFailed(String adUnitId, MoPubErrorCode errorCode);
 }
